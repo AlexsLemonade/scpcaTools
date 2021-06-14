ba5e82f546ce433bfd3a6afb4c97c1025d760341
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-06-14 18:58:23 -0500

    fix errors in cellranger function

--- R/read_cellranger.R
+++ R/read_cellranger.R
@@ -10,14 +10,13 @@ read_cellranger <- function(quant_dir) {
 
   sce <- DropletUtils::read10xCounts(file.path(quant_dir,
                                               "outs", "filtered_feature_bc_matrix.h5"),
-                                    sample.names = .x,
+                                    sample.names = basename(quant_dir),
                                     col.names = TRUE)
 
   # for consistency with other quantifiers:
   # change the column names just the barcode value, which is the first part of the barcode name
   # drop colData
-  colnames(sce) <- stringr::str_extract(colnames(x), "^([ACGT]+)")
-  colData(x) <- NULL
-  names(cellranger_sces) <- quant_ids
+  colnames(sce) <- stringr::str_extract(colnames(sce), "^([ACGT]+)")
+  colData(sce) <- NULL
   return(sce)
 }
