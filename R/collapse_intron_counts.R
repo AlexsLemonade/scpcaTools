6e0b95acdbd2014188054533979efb2ff0160563
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-06-14 17:36:04 -0500

    update collapse intron

--- R/collapse_intron_counts.R
+++ R/collapse_intron_counts.R
@@ -8,17 +8,19 @@
 #' @export
 #'
 #' @examples
-collapse_intron_counts <- function(counts, which_counts, intron_metadata){
+collapse_intron_counts <- function(counts, which_counts = c("splice", "intron"), intron_metadata){
 
-  if(which_counts === c("U", "S")) {
+  which_counts <- match.arg(which_counts)
+
+  if(which_counts == "intron") {
     shared_genes <- intersect(row.names(counts), rownames(intron_metadata))
-    # replace row names with -I appended with corresponding spliced gene
+     # replace row names with -I appended with corresponding spliced gene
     row.names(counts)[which(row.names(counts) %in% shared_genes)] <- intron_metadata[shared_genes, "spliced"]
     # aggregate Matrix counts by gene name
     counts <- Matrix.utils::aggregate.Matrix(counts, row.names(counts))
 
-  } else if (which_counts == c("S")) {
-    intron_rows <- grep("-I", row.names(counts))
+  } else if (which_counts == "splice") {
+    intron_rows <- counts[grep("-I", row.names(counts)),]
     counts <- counts[-intron_rows,]
   }
   sce <- SingleCellExperiment::SingleCellExperiment(list(counts = counts))
