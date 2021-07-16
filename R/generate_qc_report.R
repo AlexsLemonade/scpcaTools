810699b2422f437d1923c1c689f4d1206501a39e
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-07-16 13:21:48 -0400

    Documentation updates

--- R/generate_qc_report.R
+++ R/generate_qc_report.R
@@ -1,20 +1,23 @@
 #' Generate a QC report from a SingleCellExperiment object
 #'
-#' @param sce
-#' @param sample_name
-#' @param outfile
-#' @param ...
+#' @param sce A SingleCellExperiment object that the report will describe
+#' @param sample_name The name of the sample
+#' @param outfile The output file path that will be created.
+#'   If the file name does not include an extension, ".html" will be added automatically.
+#'   Any directories in the path will be created as needed.
 #'
-#' @return The path of the output file
+#' @return The full path of the output file
 #' @export
 #'
 #'
 #' @examples
+#' \dontrun{
+#' generate_qc_report(my_sce, "Sample 1", output = "reports/sample1_report.html")
+#' }
 #'
 generate_qc_report <- function(sce,
                                sample_name,
-                               output = NULL,
-                               ...){
+                               output = NULL){
   if(!inherits(sce,"SingleCellExperiment")){
     stop("`sce` must be a SingleCellExperiment object.")
   }
