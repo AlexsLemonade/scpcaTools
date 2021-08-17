1b0227b263fa25eb88fa485bdc48c5ede9d4cd13
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-08-17 15:52:34 -0400

    Change to take SCE as input

--- R/add_altexp.R
+++ R/add_altexp.R
@@ -1,27 +1,37 @@
-#' Add an Alternative Experiment to a SingleCellExperiment by "left join"
+#' Add an alternative experiment to a SingleCellExperiment by "left join"
 #'
-#' @param sce
-#' @param feature_counts
-#' @param feature_name
-#' @param feature_metadata
+#' This function takes SingleCellExperiment (SCE) and adds an Alternative Experiment (AltExp) to it.
+#' If the two experiments do not have the same set of cells, this will use the first
+#' SCE as the base set, adding columns (cells) to the AltExp so that the matrices match on that dimension.
+#' Columns (cells) that are not found in the base experiment are discarded.
 #'
-#' @return
+#' @param sce a SingleCellExperiment object
+#' @param alt_exp a second SummarizedExperiment to add as an alternative experiment
+#'   Most likely, this will also be a SingleCellExperiment, but it does not have to be.
+#' @param alt_name The name to use for the alternative experiment slot
+#'
+#' @return A SingleCellExperiment with an AltExp slot.
 #' @export
 #'
-#' @examples
-add_altexp <- function(sce, feature_counts, feature_name, feature_metadata = NULL){
-  cell_barcodes <- colnames(sce)
-  feature_cell_barcodes <- colnames(feature_counts)
-  features <- rownames(feature_counts)
-  # perform "left join": create blank columns for cells without feature counts
-  feature_missing_cells <- cell_barcodes[!cell_barcodes %in% feature_cell_barcodes]
+#' @import SingleCellExperiment
+#'
+merge_altexp <- function(sce, alt_exp, alt_name ){
+  sce_cells <- colnames(sce)
+  alt_cells <- colnames(alt_exp)
+  alt_rows <- rownames(alt_exp)
+  # perform "left join": create blank columns for cells without alt counts
+  alt_missing_cells <- sce_cells[!sce_cells %in% alt_cells]
   empty_counts <- Matrix::Matrix(0,
-                                 nrow = length(features),
-                                 ncol = length(feature_missing_cells),
-                                 dimnames = list(features, feature_missing_cells))
-  feature_counts_all <- cbind(feature_counts, empty_counts)
-  feature_counts_all <- feature_counts_all[, cell_barcodes]
-  altExp(sce, feature_name) <- SingleCellExperiment(assays = list(counts = feature_counts_all),
-                                                    metadata = feature_metadata)
+                                 nrow = length(alt_rows),
+                                 ncol = length(alt_missing_cells),
+                                 dimnames = list(alt_rows, alt_missing_cells))
+  alt_counts_all <- cbind(counts(alt_exp), empty_counts)
+  # sort to original order (dropping alt-only cells)
+  alt_counts_all <- alt_counts_all[, cell_barcodes]
+  altExp(sce, alt_name) <- SingleCellExperiment(assays = list(counts = alt_counts_all),
+                                                metadata = metadata(alt_exp))
+
+  message(glue::glue("{ncol(alt_exp)} cells with Alternative Expression counts were used",
+                     " and {length(alt_missing_cells)} blank cells were added with zero counts."))
   return(sce)
 }
