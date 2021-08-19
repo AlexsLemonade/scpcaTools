758268e1c4f80dd3f41bfab94ee19f05d2d1d0ce
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-08-19 15:54:34 -0400

    Update R/merge_altexp.R
    Co-authored-by: Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>

--- R/merge_altexp.R
+++ R/merge_altexp.R
@@ -27,6 +27,11 @@ merge_altexp <- function(sce, alt_exp, alt_name){
   sce_cells <- colnames(sce)
   alt_cells <- colnames(alt_exp)
   alt_rows <- rownames(alt_exp)
+  
+  # check that at least 50% of cells in alt experiment are found in original sce
+  if(length(intersect(sce_cells, alt_cells)) < length(alt_cells)*0.20){
+    warning("Alternative experiment has less than 20% overlap with SingleCellExperiment.")
+  }
   # perform "left join": create blank columns for cells without alt counts
   alt_missing_cells <- sce_cells[!sce_cells %in% alt_cells]
   empty_counts <- Matrix::Matrix(0,
