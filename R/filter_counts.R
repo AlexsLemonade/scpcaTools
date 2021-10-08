a8051f5997d7ea92c6481c26e754dd31c5a5d21b
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-10-08 10:34:57 -0500

    add seed to arguments for filter counts

--- R/filter_counts.R
+++ R/filter_counts.R
@@ -3,6 +3,7 @@
 #' @param sce SingleCellExperiment with unfiltered gene x cell counts matrix.
 #' @param fdr_cutoff FDR cutoff to use for DropletUtils::emptyDrops.
 #'   Default is 0.01.
+#' @param seed A random seed for reproducibility.
 #' @param ... Any arguments to be passed into DropletUtils::emptyDrops.
 #'
 #' @return SingleCellExperiment with filtered gene x cell matrix.
@@ -12,7 +13,9 @@
 #' \dontrun{
 #' filter_counts(sce = sce_object)
 #' }
-filter_counts <- function(sce, fdr_cutoff = 0.01, ...) {
+filter_counts <- function(sce, fdr_cutoff = 0.01, seed = NULL, ...) {
+
+  set.seed(seed)
 
   if(!is(sce,"SingleCellExperiment")){
     stop("Input must be a SingleCellExperiment object.")
