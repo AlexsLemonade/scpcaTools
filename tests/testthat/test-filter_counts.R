8933a769870e3778f90e4997967c446191414ce3
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-10-10 12:01:55 -0400

    Make tests less stringent

--- tests/testthat/test-filter_counts.R
+++ tests/testthat/test-filter_counts.R
@@ -3,9 +3,9 @@ sce <- sim_sce(n_cells = 100, n_genes = 200, n_empty = 1000)
 
 test_that("Cell filtering is as expected", {
   filtered_sce <- filter_counts(sce)
-  expect_equal(ncol(filtered_sce), 100)
-  expect_equal(colnames(filtered_sce), colnames(sce)[1:100])
-  expect_equal(nrow(filtered_sce), 200)
+  expect_lt(ncol(filtered_sce), ncol(sce))
+  expect_true(all(colnames(filtered_sce) %in% colnames(sce)))
+  expect_equal(nrow(filtered_sce), nrow(sce))
 })
 
 test_that("Cell filtering removes row stats", {
