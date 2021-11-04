2931e263791a327329d01ab53ab82a068c7b14c1
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-11-04 15:41:23 -0400

    Update renv.lock with extra dependencies

---
+++ dependencies.R
@@ -0,0 +1,9 @@
+# additional packages from suggests
+# and/or for the Docker image
+
+library(AnnotationHub)
+library(BSgenome)
+library(eisaR)
+library(ensembldb)
+library(fishpond)
+library(GenomicFeatures)
