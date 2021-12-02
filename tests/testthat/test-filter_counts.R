282d195d980ccf230d6aed2d8f29bf09b1271030
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-12-02 14:20:58 -0600

    test use of emptyDropsCellRanger

--- tests/testthat/test-filter_counts.R
+++ tests/testthat/test-filter_counts.R
@@ -1,8 +1,15 @@
 set.seed(1665)
-sce <- sim_sce(n_cells = 100, n_genes = 200, n_empty = 1000)
+sce <- sim_sce(n_cells = 1000, n_genes = 200, n_empty = 100)
 
-test_that("Cell filtering is as expected", {
+test_that("Cell filtering with emptyDropsCellRanger() is as expected", {
   filtered_sce <- filter_counts(sce)
+  expect_lt(ncol(filtered_sce), ncol(sce))
+  expect_true(all(colnames(filtered_sce) %in% colnames(sce)))
+  expect_equal(nrow(filtered_sce), nrow(sce))
+})
+
+test_that("Cell filtering with emptyDrops() is as expected", {
+  filtered_sce <- filter_counts(sce, cr_like = FALSE)
   expect_lt(ncol(filtered_sce), ncol(sce))
   expect_true(all(colnames(filtered_sce) %in% colnames(sce)))
   expect_equal(nrow(filtered_sce), nrow(sce))
