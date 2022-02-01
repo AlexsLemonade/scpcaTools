6c7fbe75f7a8223c0ea4acc860935d3b99a3e087
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2022-02-01 11:16:09 -0500

    Apply suggestions from code review: comment updates
    Co-authored-by: Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>

--- R/add_cellhash_calls.R
+++ R/add_cellhash_calls.R
@@ -28,13 +28,13 @@ add_hashsample_table <- function(sce, hashsample_table, altexp_id = "cellhash"){
   if(!altexp_id %in% altExpNames(sce)){
     stop(glue::glue("altexp_id `{altexp_id}` not found as an experiment in sce"))
   }
-  # check that input is a SingleCellExperiment
+  # check that hashsample_table is a data frame containing a sample_id and barcode_id column
   if(!(is(hashsample_table, "data.frame") &&
      all(c("barcode_id", "sample_id") %in% colnames(hashsample_table)))){
     stop("hashsample_table must be a data frame with columns `barcode_id` and `sample_id`")
   }
 
-  # get barcodes from sce if needed
+  # get rowData from sce and add barcode_id column if needed
   altexp_rowdata <- rowData(altExp(sce, altexp_id)) |>
     as.data.frame()
   if (!"barcode_id" %in% colnames(altexp_rowdata)){
