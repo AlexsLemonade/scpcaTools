6b5caed4a2535fecc0bf8f494f173a4d04ad31ee
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2022-02-03 15:40:39 -0500

    Add function for seurat calls

--- R/add_cellhash_calls.R
+++ R/add_cellhash_calls.R
@@ -96,7 +96,7 @@ add_cellhash_ids <- function(sce, hashsample_table,
 #' @param ... Other arguments to pass to DropletUtils::hashedDrops
 #'
 #'
-#' @return SingleCellExperiment with a `colData` column `hashedDrops_id` containing the confident demultiplexing calls.
+#' @return SingleCellExperiment with a `colData` column `hashedDrops_sampleid` containing the confident demultiplexing calls.
 #'   Other results from `DropletUtils::hashedDrops()` are included in the `rowData` for the cellhash `altExp`,
 #'   with the prefix `hashedDrops_`. See [DropletUtils::hashedDrops()] for the contents of these fields.
 #'
@@ -134,10 +134,10 @@ add_demux_hashedDrops <- function(sce, altexp_id = "cellhash", ...){
   stopifnot(all.equal(rownames(hash_result), colnames(sce)))
 
   # extract results with sample ids for main rowData table
-  hashedDrops_bestid = sample_ids[hash_result$Best]
-  hashedDrops_id = ifelse(hash_result$Confident,
-                          hashedDrops_bestid,
-                          NA_character_)
+  hashedDrops_bestsample <- sample_ids[hash_result$Best]
+  hashedDrops_sampleid <- ifelse(hash_result$Confident,
+                                 hashedDrops_bestsample,
+                                 NA_character_)
 
 
   ### add all table results to altExp rowData
@@ -148,10 +148,93 @@ add_demux_hashedDrops <- function(sce, altexp_id = "cellhash", ...){
   colData(altExp(sce, altexp_id))[, colnames(hash_result)] <- hash_result
 
   # add labeled results
-  altExp(sce, altexp_id)$hashedDrops_id <- hashedDrops_id
-  altExp(sce, altexp_id)$hashedDrops_bestid <- hashedDrops_bestid
+  altExp(sce, altexp_id)$hashedDrops_sampleid <- hashedDrops_sampleid
+  altExp(sce, altexp_id)$hashedDrops_bestsample <- hashedDrops_bestsample
   # add id to main sce table
-  sce$hashedDrops_id <- hashedDrops_id
+  sce$hashedDrops_sampleid <- hashedDrops_sampleid
 
   return(sce)
+}
+
+
+#' Add cellhash demultiplexing results using Seurat::HTODemux()
+#'
+#' @param sce SingleCellExperiment object.
+#' @param altexp_id The name of the alternative experiment that contains the cellhash data.
+#'   Default value: "cellhash"
+#' @param ... Other arguments to pass to Seurat::HTODemux
+#'
+#'
+#' @return SingleCellExperiment with a `colData` column `htodemux_id` containing the confident demultiplexing calls.
+#'   Other results from `Seurat::HTODemuxs()` are included in the `rowData` for the cellhash `altExp`,
+#'   with the prefix `HTODemux_`. See [Seurat::HTODemux()] for the contents of these fields.
+#'
+#' @import SingleCellExperiment
+#' @importFrom rlang .data
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   # add cell calls from Seurat::HTODemux()
+#'   add_demux_seurat(sce = sce)
+#' }
+add_demux_seurat <- function(sce, altexp_id = "cellhash", ...){
+  if (!requireNamespace("Seurat", quietly = TRUE)) {
+    warning("The Seurat package must be installed to use Seurat::HTODemux(). Returning unmodified sce object")
+    return(sce)
+  }
+
+  # check that input is a SingleCellExperiment
+  if(!is(sce, "SingleCellExperiment")){
+    stop("sce must be a SingleCellExperiment object")
+  }
+  if(!altexp_id %in% altExpNames(sce)){
+    stop(glue::glue("altexp_id `{altexp_id}` not found as an experiment in sce"))
+  }
+  # get ids to use: if sample ids are present, use those,
+  #  otherwise barcodes or rownames, in that order
+  sample_ids <- rowData(altExp(sce, altexp_id))$sample_id
+  if(is.null(sample_ids)){
+    sample_ids <- rowData(altExp(sce, altexp_id))$barcode_id
+  }
+  if (is.null(sample_ids)){
+    warning("No sample ids are present for demux results, using row names")
+    sample_ids <- rownames(altExp(sce, altexp_id))
+  }
+  # label by row names for later selection
+  names(sample_ids) <- rownames(altExp(sce, altexp_id))
+
+  # convert to Seurat object (quietly)
+  suppressMessages({
+    seurat_obj <- Seurat::CreateSeuratObject(counts(sce))
+    seurat_obj[["HTO"]] <- Seurat::CreateAssayObject(counts = counts(altExp(sce, "cellhash")))
+    # seurat requires normalized HTO data
+    seurat_obj <- Seurat::NormalizeData(seurat_obj, assay = "HTO", normalization.method = "CLR")
+    # calculate cellhash results
+    try(seurat_obj <- Seurat::HTODemux(seurat_obj))
+  })
+  # columns that HTODemux adds to results
+  hto_cols <- c("HTO_maxID",
+                "HTO_secondID",
+                "HTO_margin",
+                "HTO_classification",
+                "HTO_classification.global",
+                "hash.ID")
+
+  seurat_demux <- seurat_obj@meta.data |>
+    dplyr::select(all_of(hto_cols))|>
+    # rename colums with htodemux_ prefix, removing HTO_ if present
+    dplyr::rename_with(~ stringr::str_replace(.x, "(HTO_)?(.+)", "HTODemux_\\2")) |>
+    dplyr::mutate(HTODemux_maxsample = sample_ids[HTODemux_maxID],
+                  HTODemux_sampleid = ifelse(HTODemux_hash.ID %in% c("Doublet","Negative"),
+                                             NA_character_,
+                                             sample_ids[HTODemux_hash.ID]))
+
+    ## add htodemux columns to altExp
+    colData(altExp(sce, altexp_id))[, colnames(seurat_demux)] <- seurat_demux
+    # add id to main sce table
+    sce$HTODemux_sampleid <- seurat_demux$HTODemux_sampleid
+
+    return(sce)
 }
