3717367681ebbc1534f39c4e138981f8d025d123
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-07-05 15:05:46 -0500

    check column and row Data between sce and anndata

--- tests/testthat/test-sce_to_anndata.R
+++ tests/testthat/test-sce_to_anndata.R
@@ -1,11 +1,18 @@
 set.seed(1665)
 sce <- sim_sce(n_cells = 100, n_genes = 200, n_empty = 0)
+colData(sce) <- DataFrame("test_column" = sample(0:10, 100, rep= TRUE))
+rowData(sce) <- DataFrame("test_row" = sample(0:10, 200, rep = TRUE))
 
 test_that("Conversion of SCE to AnnData works as expected", {
   anndata_file <- "test_anndata.h5"
 
   # test that the H5 file is created
   expect_snapshot(sce_to_anndata(sce, anndata_file))
+
+  # some tests that anndata object contains col/rowData found in SCE
+  anndata_object <- sce_to_anndata(sce, anndata_file)
+  expect_equal(colnames(colData(sce)), colnames(anndata_object$obs))
+  expect_equal(colnames(rowData(sce)), colnames(anndata_object$var))
 
   # check that inputting an improper file name causes a failure
   anndata_bad_file <- "test_anndata"
