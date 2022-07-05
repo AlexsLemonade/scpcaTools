c957bb23ed5900d415d483111752b2be2c00ac7d
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-07-05 14:03:25 -0500

    add checks and comments

--- R/sce_to_anndata.R
+++ R/sce_to_anndata.R
@@ -1,17 +1,55 @@
 
-sce_to_anndata <- function(sce){
+#' Convert SingleCellExperiment objects to AnnData file stored as HDF5 file
+#'
+#' @param sce SingleCellExperiment object to be converted to AnnData
+#' @param anndata_file Name of
+#'
+#' @return Converted AnnData object
+#'
+#' @import SingleCellExperiment
+#' @import reticulate
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' sce_to_anndata(sce = sce_object,
+#'                anndata_file = "test_anndata.h5")
+#' }
+sce_to_anndata <- function(sce, anndata_file){
 
   if(!is(sce,"SingleCellExperiment")){
     stop("Input must be a SingleCellExperiment object.")
   }
 
-  anndata <- basilisk::basiliskRun(fun = function(sce){
+  # check that filename is in the proper format for writing h5
+  if(!(stringr::str_ends(anndata_file, ".hdf5|.h5"))){
+    stop("anndata file must end in either `.hdf5` or .`h5`")
+  }
 
-    adata <- zellkonverter::SCE2AnnData(sce)
+  # remove miQC model from metadata
+  if(!is.null(metadata(sce)$miQC_model)){
+    metadata(sce)$miQC_model <- NA
+    warning("miQC model cannot be converted between SCE and AnnData.")
+  }
 
-    zellkonverter::AnnData2SCE(adata)
+  anndata_object <- basilisk::basiliskRun(fun = function(sce,
+                                                         anndata_file = NULL){
+    # import anndata as adata
+    adata <- reticulate::import("anndata")
 
-  }, env = zellkonverterAnnDataEnv, sce = sce)
+    # create anndata object from SCE
+    rna_adata <- zellkonverter::SCE2AnnData(sce)
 
-  return(anndata)
+    # write out adata object
+    rna_adata$write(filename = anndata_file)
+
+    # return adata object
+    rna_adata
+
+  }, env = zellkonverter::zellkonverterAnnDataEnv,
+  sce = sce,
+  anndata_file = anndata_file)
+
+  return(anndata_object)
 }
