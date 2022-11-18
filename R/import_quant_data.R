0178d821991f50b4456f5d2740016aa1d9f038cd
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-11-18 16:34:53 -0600

    propagate changes to import_quant_data and readme

--- R/import_quant_data.R
+++ R/import_quant_data.R
@@ -4,15 +4,13 @@
 #'
 #' @param quant_dir Path to directory where output files are located.
 #' @param tool Type of tool used to create files (alevin, alevin-fry, cellranger, or kallisto).
-#' @param mtx_format Logical indicating if input data is in matrix market format.
-#'   Default is FALSE.
-#' @param intron_mode Logical indicating if the files included alignment to intronic regions.
-#'   Default is FALSE.
+#' @param include_unspliced Whether or not to include the unspliced reads in the counts matrix.
+#'   If TRUE, the main "counts" assay will contain unspliced reads and spliced reads and an additional "spliced"
+#'   assay will contain spliced reads only. If TRUE, requires that data has been aligned to a reference contianing
+#'   spliced and unspliced reads.
+#'   Default is TRUE.
 #' @param usa_mode Logical indicating if Alevin-fry was used, if the USA mode was invoked.
 #'   Default is FALSE.
-#' @param which_counts If intron_mode is TRUE, which type of counts should be included,
-#'   only counts aligned to spliced cDNA ("spliced") or all spliced and unspliced cDNA ("unspliced").
-#'   Default is "spliced".
 #' @param filter Logical indicating whether or not to filter the counts matrix.
 #'   Filtering is performed using DropletUtils::emptyDrops and cannot be performed with Cellranger.
 #' @param fdr_cutoff FDR cutoff to use for DropletUtils::emptyDrops.
@@ -33,7 +31,7 @@
 #' # with alignment to cDNA only and including counts for spliced cDNA only
 #' import_quant_data(quant_dir,
 #'                   tool = "alevin-fry",
-#'                   intron_mode = FALSE,
+#'                   include_unspliced = FALSE,
 #'                   usa_mode = TRUE)
 #'
 #' # read in single-nuclei RNA-seq data processed using alevin-fry in
@@ -42,7 +40,6 @@
 #' import_quant_data(quant_dir,
 #'                   tool = "alevin-fry",
 #'                   usa_mode = TRUE,
-#'                   which_counts = "unspliced",
 #'                   filter = TRUE)
 #'
 #' # read in single-nuclei RNA-seq data processed using kallisto with
@@ -50,16 +47,12 @@
 #' # and perform filtering
 #' import_quant_data(quant_dir,
 #'                   tool = "kallisto",
-#'                   intron_mode = TRUE,
-#'                   which_counts = "unspliced",
 #'                   filter = TRUE)
 #' }
 #'
 import_quant_data <- function(quant_dir,
                               tool = c("cellranger", "alevin", "alevin-fry", "kallisto"),
-                              which_counts = c("spliced", "unspliced"),
-                              mtx_format = FALSE,
-                              intron_mode = FALSE,
+                              include_unspliced = TRUE,
                               usa_mode = FALSE,
                               filter = FALSE,
                               fdr_cutoff = 0.01,
@@ -73,8 +66,8 @@ import_quant_data <- function(quant_dir,
   }
 
   # checks for intron_mode and usa_mode
-  if(!is.logical(intron_mode)){
-    stop("intron_mode must be set as TRUE or FALSE")
+  if(!is.logical(include_unspliced)){
+    stop("include_unspliced must be set as TRUE or FALSE")
   }
   if(!is.logical(usa_mode)){
     stop("usa_mode must be set as TRUE or FALSE")
@@ -82,19 +75,13 @@ import_quant_data <- function(quant_dir,
   if(!is.logical(filter)){
     stop("filter must be set as TRUE or FALSE")
   }
-  if(!is.logical(mtx_format)){
-    stop("mtx_format must be set as TRUE or FALSE")
-  }
 
   # check that usa_mode and intron_mode are used with the proper tools
   if(usa_mode & tool %in% c("cellranger", "alevin", "kallisto")){
     stop("USA mode only compatible with alevin-fry.")
   }
-  if(intron_mode & tool %in% c("cellranger")){
-    stop("Intron mode not compatible with cellranger.")
-  }
-  if(usa_mode & intron_mode){
-    stop("Can only read counts using either usa mode or intron mode.")
+  if(include_unspliced & tool %in% c("cellranger")){
+    stop("Include unspliced not compatible with cellranger.")
   }
 
   # check that filter is not used with cellranger
@@ -111,9 +98,9 @@ import_quant_data <- function(quant_dir,
   }
 
   if (tool %in% c("alevin-fry", "alevin")){
-    sce <- read_alevin(quant_dir, mtx_format, intron_mode, usa_mode, which_counts, tech_version)
+    sce <- read_alevin(quant_dir, include_unspliced, usa_mode, tech_version)
   } else if (tool == "kallisto") {
-    sce <- read_kallisto(quant_dir, intron_mode, which_counts)
+    sce <- read_kallisto(quant_dir, include_unspliced)
   } else if (tool == "cellranger") {
     sce <- read_cellranger(quant_dir)
   }
