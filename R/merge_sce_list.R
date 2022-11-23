aa520cec936122b128f4dd8696a27d9c22b92ec0
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-11-23 14:10:55 -0500

    passing tests galore

--- R/merge_sce_list.R
+++ R/merge_sce_list.R
@@ -12,7 +12,13 @@
 #'
 #' @param sce_list A named list of SingleCellExperiment objects
 #' @param batch_column A character value giving the resulting colData column name
-#'  to differentiate originating SingleCellExperiment objects Default value is `batch`.
+#'  to differentiate originating SingleCellExperiment objects. Often these values
+#'  are library IDs. Default value is `library_id`.
+#' @param barcode_column A character value giving the colData column name that
+#'  stores cell barcodes. If this column does not yet exist it will be created. Default
+#'  is `barcode`.
+#'  The name of the column to be created (or not if it
+#'   already exists) in the colData slot to store cell barcodes
 #' @param retain_coldata_cols A vector of colData columns which should be retained
 #'  in the main experiment of the final merged SCE object.
 #' @param preserve_rowdata_cols A vector of column names that appear in originating
@@ -27,14 +33,16 @@
 #'
 #' @import SingleCellExperiment
 merge_sce_list <- function(sce_list = list(),
-                           batch_column = "batch",
+                           batch_column = "library_id",
+                           barcode_column = "barcode",
                            retain_coldata_cols = c("sum",
                                                    "detected",
                                                    "total",
                                                    "subsets_mito_sum",
                                                    "subsets_mito_detected",
                                                    "subsets_mito_percent",
-                                                   "miQC_pass"),
+                                                   "miQC_pass",
+                                                   "prob_compromised"),
                            preserve_rowdata_cols = NULL) {
 
   # Check `sce_list`----------------------
@@ -47,8 +55,8 @@ merge_sce_list <- function(sce_list = list(),
     return(sce_list)
   }
 
-  # Check input vectors ----------------
-  if(length(retain_coldata_cols) == 0) {
+  # Check `retain_coldata_cols` ----------------
+  if (length(retain_coldata_cols) == 0) {
     warning("All colData will be removed from the the merged SCE.
             Please check that `retain_coldata_cols` was correctly specified.")
   }
@@ -60,13 +68,34 @@ merge_sce_list <- function(sce_list = list(),
     purrr::map(rownames) |>
     purrr::reduce(intersect)
 
+  if (length(shared_features) == 0) {
+    stop("There are no shared features among provided SCE objects.
+         They cannot be merged.")
+  }
+
+  # Second, determine all the column names that are present in any SCE so it can
+  #  be created in any missing SCEs with `NA` values
+  all_colnames <- purrr::map(sce_list, ~names(colData(.))) |>
+    unlist() |>
+    unname() |>
+    unique()
+
+  # Check that the `retain_coldata_cols` are present in at least one SCE, and
+  #  error if the column exists nowhere.
+  if (!(any(retain_coldata_cols %in% all_colnames))) {
+    stop("Error: The provided `retain_coldata_cols` are not present in any SCEs.")
+  }
+
   # Prepare SCEs
   sce_list <- purrr::imap(sce_list,
                           prepare_sce_for_merge,
                           batch_column,
+                          barcode_column,
                           shared_features,
-                          retain_coldata_cols,
-                          preserve_rowdata_cols)
+                          # ensure that barcodes are retained
+                          c(barcode_column, retain_coldata_cols),
+                          preserve_rowdata_cols,
+                          all_colnames)
 
 
   # Create the merged SCE from the processed list ------------------
@@ -86,28 +115,31 @@ merge_sce_list <- function(sce_list = list(),
 #' @param sce_name The name of the SCE object
 #' @param batch_column The name of the batch column will which be added to the
 #'   colData slot
+#' @param barcode_column The name of the column to be created (or not if it
+#'   already exists) in the colData slot to store cell barcodes
 #' @param shared_features A vector of features (genes) that all SCEs to be merged
 #'   have in common
 #' @param retain_coldata_cols A vector of columns to retain in the colData slot
 #' @param preserve_rowdata_cols A vector of rowData columns which should not be
 #'   renamed
+#' @param expected_coldata_names A vector of column names that are expected to be
+#'   present in the SCE. Any missing columns will be created with `NA` values.
 #'
 #' @return An updated SCE that is prepared for merging
 prepare_sce_for_merge <- function(sce,
                                   sce_name,
                                   batch_column,
+                                  barcode_column,
                                   shared_features,
                                   retain_coldata_cols,
-                                  preserve_rowdata_cols) {
+                                  preserve_rowdata_cols,
+                                  expected_coldata_names) {
 
   # Current functionality does not retain any present altExps
   sce <- removeAltExps(sce)
 
   # Subset to shared features
   sce <- sce[shared_features,]
-
-  # Add `sce_name` to colData row names so cell barcodes can be mapped to originating SCE
-  rownames(colData(sce)) <- glue::glue("{rownames(colData(sce))}-{sce_name}")
 
   ##### rowData #####
   # Add `sce_name` ID to rowData column names except for those
@@ -121,16 +153,42 @@ prepare_sce_for_merge <- function(sce,
   )
 
   ##### colData #####
-  # Retain only the columns present in `retain_coldata_cols`
-  coldata_names <- names(colData(sce))
-  if (!(all(retain_coldata_cols %in% coldata_names))) {
-    stop("Error: Provided columns to retain are not present
-            in all SCE objects.")
+  observed_coldata_names <- names(colData(sce))
+
+  # If the barcode column is not already present, create it
+  if (!(barcode_column %in% observed_coldata_names)) {
+    colData(sce)[,barcode_column] <- rownames(colData(sce))
+    observed_coldata_names <- c(observed_coldata_names, barcode_column)
   }
+
+  # Ensure barcode_column is expected and retained
+  if (!(barcode_column %in% expected_coldata_names)) {
+    expected_coldata_names <- c(expected_coldata_names, barcode_column)
+  }
+  if (!(barcode_column %in% retain_coldata_cols)) {
+    retain_coldata_cols <- c(retain_coldata_cols, barcode_column)
+  }
+
+  # Ensure all columns are present in all SCEs by adding `NA` columns as needed
+  missing_columns <- setdiff(expected_coldata_names, observed_coldata_names)
+  for (missing_col in missing_columns) {
+    # Create the missing column only if it should be retained
+    if (missing_col %in% retain_coldata_cols) {
+      colData(sce)[[missing_col]] <- NA
+    }
+  }
+
+  # Retain only the columns present in `retain_coldata_cols`
   colData(sce) <- colData(sce) |>
     as.data.frame() |>
     dplyr::select( dplyr::all_of(retain_coldata_cols) ) |>
     S4Vectors::DataFrame()
+  # this approach doesn't work when retain is length 1:
+  #colData(sce) <- colData(sce)[, retain_coldata_cols]
+
+  # Add `sce_name` to colData row names so cell barcodes can be mapped to originating SCE
+  original_rownames <- rownames(colData(sce))
+  rownames(colData(sce)) <- glue::glue("{rownames(colData(sce))}-{sce_name}")
 
   # Add batch column
   sce[[batch_column]] <- sce_name
--- tests/testthat/test-merge_sce_list.R
+++ tests/testthat/test-merge_sce_list.R
@@ -1,22 +1,14 @@
-# helper function for tests:
-# Add more info to the SCEs
+# helper function to add more info to a simulated SCE ----
 add_sce_data <- function(sce) {
-  colData(sce) <- colData(sce) |>
-    as.data.frame() |>
-    dplyr::mutate(
-      sum = runif(nrow(colData(sce))),
-      detected = runif(nrow(colData(sce))),
-      total = runif(nrow(colData(sce)))
-    ) |>
-    S4Vectors::DataFrame()
 
-  rowData(sce) <- rowData(sce) |>
-    as.data.frame() |>
-    dplyr::mutate(
-      gene_names = rownames(sce),
-      other_column = runif(nrow(rowData(sce)))
-    ) |>
-    S4Vectors::DataFrame()
+  # add some coldata columns
+  colData(sce)[["sum"]] <- runif(nrow(colData(sce)))
+  colData(sce)[["detected"]] <- runif(nrow(colData(sce)))
+  colData(sce)[["total"]] <- runif(nrow(colData(sce)))
+
+  # add some rowdata columns
+  rowData(sce)[["gene_names"]] <- rownames(rowData(sce))
+  rowData(sce)[["other_column"]] <- runif(nrow(rowData(sce)))
 
   # Copy counts -> logcounts just to make sure the assay is retained
   logcounts(sce) <- counts(sce)
@@ -24,20 +16,122 @@ add_sce_data <- function(sce) {
 }
 
 
-test_that("merging SCEs with matching genes works as expected", {
+# Generate some shared data for testing `prepare_sce_for_merge()` ---
+set.seed(1665)
+total_cells <- 24 # divisible by 3
+total_genes <- 12 # number of months intentionally.
+sce <- add_sce_data(
+  sim_sce(n_cells = total_cells, n_genes = total_genes, n_empty = 0)
+)
+sce_name <- "sce_object"
+batch_column <- "batch" # not the default
+barcode_column <- "barcode_col" # not the default
+shared_features <- rownames(sce)[1:10]
+retain_coldata_cols <- c("sum", "detected")
+preserve_rowdata_cols <- "gene_names"
+expected_coldata_names <- c("sum", "detected", "total")
 
-  # create three SCEs with the same number of genes
-  set.seed(1665)
-  total_cells <- 75
-  total_genes <- 50
-  sce1 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
-  sce2 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
-  sce3 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
+# Generate some shared data for testing `merge_sce_list()` ------
+sce1 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
+sce2 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
+sce3 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
+
+
+
+
+
+
+
+
+test_that("`prepare_sce_for_merge` works as expected when all columns are present", {
+
+  # Barcode is NOT explicitly included in `retain_coldata_cols` or `expected_coldata_names`
+  result_sce <- prepare_sce_for_merge(sce,
+                                      sce_name,
+                                      batch_column,
+                                      barcode_column,
+                                      shared_features,
+                                      retain_coldata_cols,
+                                      preserve_rowdata_cols,
+                                      expected_coldata_names)
+
+  expect_equal(ncol(result_sce), total_cells) # cells
+  expect_equal(nrow(result_sce), length(shared_features)) # genes
+
+  # colData names and contents:
+  expect_equal(
+    sort(names(colData(result_sce))),
+    sort(c(batch_column, barcode_column, retain_coldata_cols))
+  )
+  expect_equal(unique(result_sce[[batch_column]]), sce_name)
+  expect_equal(
+    sort(rownames(colData(result_sce))),
+    sort(c(glue::glue("{rownames(colData(sce))}-{sce_name}")))
+  )
+
+  # rowData names and contents:
+  expect_equal(
+    sort(names(rowData(result_sce))),
+    c("gene_names", paste("other_column", sce_name, sep = "-"))
+  )
+
+})
+
+
+
+test_that("`prepare_sce_for_merge` works as expected when all an expected column is missing", {
+
+  # REMOVE "detected" column first -
+  colData(sce)$detected <- NULL
+
+  result_sce <- prepare_sce_for_merge(sce,
+                                      sce_name,
+                                      batch_column,
+                                      barcode_column,
+                                      shared_features,
+                                      retain_coldata_cols,
+                                      preserve_rowdata_cols,
+                                      expected_coldata_names)
+
+  # `detected` column should exist as all NA:
+  expect_true(all(is.na(colData(result_sce)$detected)))
+
+})
+
+
+
+test_that("`prepare_sce_for_merge` works as expected when barcode column already exists", {
+
+  # create barcode
+  barcodes <- rownames(colData(sce))
+  colData(sce)$barcode_col <- barcodes
+
+  result_sce <- prepare_sce_for_merge(sce,
+                                      sce_name,
+                                      batch_column,
+                                      barcode_column,
+                                      shared_features,
+                                      retain_coldata_cols,
+                                      preserve_rowdata_cols,
+                                      expected_coldata_names)
+
+  # `barcode_col` column should have been added
+  expect_equal(colData(result_sce)$barcode_col,
+               barcodes)
+
+})
+
+
+####################################################################
+####################################################################
+
+
+test_that("merging SCEs with matching genes works as expected", {
 
   # Set up list for input to function
   sce_list <- purrr::map(
     list(
-      "sce1" =  sce1,
+      "sce1" = sce1,
       "sce2" = sce2,
       "sce3" = sce3
     ),
@@ -46,15 +140,13 @@ test_that("merging SCEs with matching genes works as expected", {
 
   # First expect some errors or warnings:
   expect_error(merge_sce_list(sce_list = unname(sce_list))) # List must be named
-  expect_warning(merge_sce_list(sce_list = list("sce1" = sce1))) # List must have >=2 SCEs for no warning
-  expect_error(merge_sce_list(sce_list = sce_list)) # Default columns aren't in these sces
-
+  expect_warning(merge_sce_list(sce_list = list("sce1" = sce1))) # this is an early return situation- List must have >=2 SCEs for no warning
 
   # Works as expected:
-  batch_column <- "nondefault_batch"
   merged_sce <- merge_sce_list(sce_list,
-                               # use a non-default batch column
+                               # use a non-default batch and barcode column names
                                batch_column = batch_column,
+                               barcode_column = barcode_column,
                                # "detected" should get removed:
                                retain_coldata_cols = c("sum", "total"),
                                # this row name should not be modified:
@@ -68,7 +160,7 @@ test_that("merging SCEs with matching genes works as expected", {
   # colData names and contents:
   expect_equal(
     sort(names(colData(merged_sce))),
-    c(batch_column, "sum", "total")
+    sort(c(batch_column, barcode_column, "sum", "total"))
   )
   expect_equal(merged_sce[[batch_column]],
                c(rep("sce1", total_cells/3),
@@ -78,7 +170,8 @@ test_that("merging SCEs with matching genes works as expected", {
   expect_equal(
     sort(rownames(colData(merged_sce))),
     sort(
-      c(glue::glue("{rownames(colData(sce1))}-sce1"),
+      c(
+      glue::glue("{rownames(colData(sce1))}-sce1"),
       glue::glue("{rownames(colData(sce2))}-sce2"),
       glue::glue("{rownames(colData(sce3))}-sce3")
     ))
@@ -107,15 +200,8 @@ test_that("merging SCEs with matching genes works as expected", {
 
 test_that("merging SCEs with different genes among input SCEs works as expected", {
 
-  # create three SCEs with the same number of genes
-  set.seed(1665)
-  total_cells <- 75
-  total_genes <- 12 # number of months, wait for it!
-  sce1 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
-  sce2 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
-  sce3 <- sim_sce(n_cells = total_cells/3, n_genes = total_genes, n_empty = 0)
-
   # rename sce2 and sce3 genes so that only 1-6 are overlapping
+  # hence, we started with 12 genes.
   rownames(sce2) <- c(rownames(sce2)[1:6],
                       month.name[1:6])
   rownames(sce3) <- c(rownames(sce3)[1:6],
@@ -142,4 +228,24 @@ test_that("merging SCEs with different genes among input SCEs works as expected"
   # correct number of genes and cells:
   expect_equal(nrow(merged_sce), 6)
   expect_equal(ncol(merged_sce), total_cells)
+})
+
+
+
+
+test_that("merging SCEs with no matching genes fails as expected", {
+
+  rownames(sce1) <- month.name # ensure different gene names entirely
+
+  # Set up list for input to function
+  sce_list <- purrr::map(
+    list(
+      "sce1" = sce1,
+      "sce2" = sce2),
+    add_sce_data
+  )
+
+  expect_error(merge_sce_list(sce_list = sce_list,
+                              # use this arg to prevent other errors
+                              retain_coldata_cols = "sum"))
 })
