cc4c605c07f026cb3d8e692c980ea24422a63d12
Stephanie <stephanie.spielman@gmail.com>
2022-12-06 17:27:15 -0500

    Update R/integrate_sces.R
    Co-authored-by: Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>

--- R/integrate_sces.R
+++ R/integrate_sces.R
@@ -133,7 +133,7 @@ integrate_fastmnn <- function(merged_sce,
 #' approach from the `harmony` package.
 #' Source: https://cran.r-project.org/web/packages/harmony/index.html
 #'
-#' @param merged_sce A combined SCE object as prepared by `scpcaTools::merge_sce_list()`.
+#' @param merged_sce A merged SCE object as prepared by `scpcaTools::merge_sce_list()`.
 #' @param batch_column The column in the combined SCE object indicating batches
 #'   being integrated.
 #' @param harmony_covariate_cols A vector of other columns to consider as
