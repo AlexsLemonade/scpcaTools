e0d65fe1522782eded6e8fb8ab5682f390271cb1
Ally Hawkins <ally.hawkins@ccdatalab.org>
2023-02-14 13:22:50 -0600

    add test report

--- tests/testthat/test-generate_qc_report.R
+++ tests/testthat/test-generate_qc_report.R
@@ -11,12 +11,25 @@ test_that("generating a qc report works", {
   file.remove(qc_file)
 })
 
+test_that("generating a qc report works with a different template", {
+
+  test_template <- system.file(file.path("rmd", "test_report.rmd"), package = "scpcaTools")
+
+  qc_file <- generate_qc_report(library_id = "TEST",
+                                unfiltered_sce = sce,
+                                filtered_sce = filt_sce,
+                                report_template = test_template)
+  expect_true(file.exists(qc_file))
+  # clean up
+  file.remove(qc_file)
+})
+
 
 test_that("generating a qc report fails with a missing template", {
   expect_error(generate_qc_report(library_id = "TEST",
-                                unfiltered_sce = sce,
-                                filtered_sce = filt_sce,
-                                rmd_template = "missing.rmd"))
+                                  unfiltered_sce = sce,
+                                  filtered_sce = filt_sce,
+                                  report_template = "missing.rmd"))
 })
 
 
