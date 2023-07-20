765fbcf3c535e00d45f81a9538f76fc9f31fcf8f
cbethell <chantebethell@gmail.com>
2023-07-20 10:10:27 -0500

    refactor clustering and update docs

--- R/cluster_sce.R
+++ R/cluster_sce.R
@@ -3,24 +3,42 @@
 #' @param sce SingleCellExperiment object that requires clustering
 #' @param seed Seed for reproducibility of clustering results
 #' @param pca_column The SingleCellExperiment column name to reference the PCA results
-#' @param k_range Range of k values to use for k-means clustering. Default: `seq(5, 25, 5)`
+#' @param cluster_type The type of clustering method to be performed - can be "kmeans", "walktrap", or "louvain"
+#' @param k The desired number of centers
 #'
 #' @return SingleCellExperiment object containing clustering results
 cluster_sce <- function(sce,
-                        seed = 2023,
                         pca_column,
-                        k_range) {
+                        cluster_type,
+                        seed = NULL,
+                        k = NULL,
+                        ...) {
 
-  for (k in k_range) {
+  # Set the seed
+  set.seed(seed)
 
-    # Set cluster name
-    cluster_name <- paste("kmeans", k, sep = "_")
-
+  # Extract PCs
+  pcs <- reducedDim(sce, pca_column)
+  if (cluster_type == "kmeans") {
     # Perform k-means clustering
-    clustering_results <- kmeans(x = reducedDim(sce, pca_column), centers = k)
-    sce[[cluster_name]] <- unname(clustering_results$cluster)
-
+    clustering_results <- bluster::clusterRows(pcs, bluster::KmeansParam(centers = k))
+  } else if (cluster_type %in% c("walktrap", "louvain")) {
+    # Grab weighting type based on cluster type
+    weighting_type <- ifelse(cluster_type == "louvain", "jaccard", "rank")
+    # Perform graph-based clustering
+    clustering_results <- bluster::clusterRows(pcs,
+                                               bluster::NNGraphParam(
+                                                 k = k,
+                                                 type = weighting_type,
+                                                 cluster.fun = cluster_type,
+                                               ))
+  } else {
+    stop("Clustering type is not valid. Please specify 'kmeans', 'walktrap' or 'louvain' clustering.")
   }
+
+  # Set column name to store results in
+  cluster_name <- paste(cluster_type, k, sep = "_")
+  sce[[cluster_name]] <- clustering_results
 
   return(sce)
 }
