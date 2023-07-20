be733ddc7f894bf60bbf62986295b36579647459
allyhawkins <allyhawkins@users.noreply.github.com>
2023-07-20 17:31:00 +0000

    Styled by Github Actions

--- tests/testthat/test-integration_metrics.R
+++ tests/testthat/test-integration_metrics.R
@@ -20,15 +20,12 @@ pcs <- reducedDim(merged_sce, "PCA")
 batches <- merged_sce$sample
 
 test_that("`filter_pcs` works as expected", {
-
   # expect rownames of returned pcs to be the same as the batch column
   named_pcs <- filter_pcs(pcs, batches)
   expect_true(all.equal(rownames(named_pcs), batches))
-
 })
 
 test_that("`filter_pcs` removes NAs as expected", {
-
   batches[1:5] <- NA # set a subset of batch labels to NAs
 
   # expect rownames to be equal to only the batches without NA
@@ -38,43 +35,38 @@ test_that("`filter_pcs` removes NAs as expected", {
 })
 
 test_that("`filter_pcs` fails without batch labels", {
-
   all_na_batches <- NA
   expect_error(filter_pcs(pcs, all_na_batches))
-
 })
 
 test_that("`downsample_pcs` works as expected", {
-
-  test_frac = 0.8
+  test_frac <- 0.8
   downsampled <- downsample_pcs(pcs, frac_cells = test_frac)
-  downsampled_pcs <-downsampled
+  downsampled_pcs <- downsampled
   downsampled_batch_labels <- rownames(downsampled)
 
   # check that the correct number of cells were downsampled
-  expect_true(length(rownames(downsampled_pcs)) == round(test_frac*length(rownames(pcs))))
-  expect_true(length(downsampled_batch_labels) == round(test_frac*length(batches)))
+  expect_true(length(rownames(downsampled_pcs)) == round(test_frac * length(rownames(pcs))))
+  expect_true(length(downsampled_batch_labels) == round(test_frac * length(batches)))
 
   # check that the columns in downsampled were in the original pcs
   expect_true(length(colnames(downsampled_pcs)) == length(colnames(pcs)))
-
 })
 
 test_that("`downsample_pcs` fails as expected", {
-
   # check that downsampling fails when providing a fraction outside of 0-1 range
   expect_error(downsample_pcs(pcs, frac_cells = 2))
   expect_error(downsample_pcs(pcs, frac_cells = 0))
-
 })
 
 test_that("`calculate_silhouette_width` works as expected", {
-
   nreps <- 5
-  asw <- calculate_silhouette_width(integrated_sce = merged_sce,
-                                    pc_name = "PCA",
-                                    nreps = 5,
-                                    batch_column = "sample")
+  asw <- calculate_silhouette_width(
+    integrated_sce = merged_sce,
+    pc_name = "PCA",
+    nreps = 5,
+    batch_column = "sample"
+  )
 
   expected_cols <- c(
     "rep", "silhouette_width", "silhouette_cluster",
@@ -91,26 +83,26 @@ test_that("`calculate_silhouette_width` works as expected", {
 
   # check that width is in expected range
   expect_true(all(asw$silhouette_width >= -1 |
-                    asw$silhouette_width <= 1))
-
+    asw$silhouette_width <= 1))
 })
 
 test_that("`calculate_silhouette_width`fails as expected", {
-
   # missing pc name
   expect_error(calculate_silhouette_width(merged_sce,
-                                          pc_name = "test_PC",
-                                          batch_column = "sample"))
+    pc_name = "test_PC",
+    batch_column = "sample"
+  ))
 
   # error in nreps
   expect_error(calculate_silhouette_width(merged_sce,
-                                          pc_name = "PCA",
-                                          nreps = "twenty",
-                                          batch_column = "sample"))
+    pc_name = "PCA",
+    nreps = "twenty",
+    batch_column = "sample"
+  ))
 
   # incorrect batch labels
   expect_error(calculate_silhouette_width(merged_sce,
-                                          pc_name = "PCA",
-                                          batch_column = "batch"))
-
+    pc_name = "PCA",
+    batch_column = "batch"
+  ))
 })
