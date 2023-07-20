5bd2f8bf32f3ab3c487d8f21224996551d4ce656
cbethell <chantebethell@gmail.com>
2023-07-20 13:17:02 -0500

    implement refactor suggestion

--- R/cluster_sce.R
+++ R/cluster_sce.R
@@ -1,44 +1,26 @@
 #' Perform clustering on a SingleCellExperiment object
 #'
 #' @param sce SingleCellExperiment object that requires clustering
+#' @param pca_name The name used to access the PCA results
+#' @param BLUSPARAM A BlusterParam object specifying the clustering algorithm to
+#' use - can be `KmeansParam` or `NNGraphParam`
+#' @param cluster_name The type of clustering method to be performed - can be
+#' "kmeans", "walktrap", or "louvain"
 #' @param seed Seed for reproducibility of clustering results
-#' @param pca_column The SingleCellExperiment column name to reference the PCA results
-#' @param cluster_type The type of clustering method to be performed - can be "kmeans", "walktrap", or "louvain"
-#' @param k The desired number of centers
 #'
 #' @return SingleCellExperiment object containing clustering results
 cluster_sce <- function(sce,
-                        pca_column,
-                        cluster_type,
+                        pca_name,
+                        BLUSPARAM,
+                        cluster_name,
                         seed = NULL,
-                        k = NULL,
                         ...) {
   # Set the seed
   set.seed(seed)
 
   # Extract PCs
-  pcs <- reducedDim(sce, pca_column)
-  if (cluster_type == "kmeans") {
-    # Perform k-means clustering
-    clustering_results <- bluster::clusterRows(pcs, bluster::KmeansParam(centers = k))
-  } else if (cluster_type %in% c("walktrap", "louvain")) {
-    # Grab weighting type based on cluster type
-    weighting_type <- ifelse(cluster_type == "louvain", "jaccard", "rank")
-    # Perform graph-based clustering
-    clustering_results <- bluster::clusterRows(
-      pcs,
-      bluster::NNGraphParam(
-        k = k,
-        type = weighting_type,
-        cluster.fun = cluster_type,
-      )
-    )
-  } else {
-    stop("Clustering type is not valid. Please specify 'kmeans', 'walktrap' or 'louvain' clustering.")
-  }
-
-  # Set column name to store results in
-  cluster_name <- paste(cluster_type, k, sep = "_")
+  pcs <- reducedDim(sce, pca_name)
+  clustering_results <- bluster::clusterRows(pcs, BLUSPARAM, ...)
   sce[[cluster_name]] <- clustering_results
 
   return(sce)
