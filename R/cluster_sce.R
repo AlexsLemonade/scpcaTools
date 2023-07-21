295bd37609ed165dd82f8ed1566b55dfcd97e952
cbethell <chantebethell@gmail.com>
2023-07-21 13:26:44 -0500

    update docs and add check for pcs

--- R/cluster_sce.R
+++ R/cluster_sce.R
@@ -3,25 +3,31 @@
 #' @param sce SingleCellExperiment object that requires clustering
 #' @param pca_name The name used to access the PCA results
 #' @param BLUSPARAM A BlusterParam object specifying the clustering algorithm to
-#' use - can be `KmeansParam` or `NNGraphParam`
-#' @param cluster_name The type of clustering method to be performed - can be
-#' "kmeans", "walktrap", or "louvain"
+#' use
+#' @param cluster_column_name The name of the column to store the clustering
+#' results in the SCE
 #' @param seed Seed for reproducibility of clustering results
 #'
 #' @return SingleCellExperiment object containing clustering results
 cluster_sce <- function(sce,
                         pca_name,
                         BLUSPARAM,
-                        cluster_name,
+                        cluster_column_name,
                         seed = NULL,
                         ...) {
   # Set the seed
   set.seed(seed)
 
+  # Check that the PCs are present in the SingleCellExperiment object
+  if (!pca_name %in% reducedDimNames(sce)) {
+    stop("The provided `pca_name` cannot be found in the reduced dimensions of
+         the SingleCellExperiment object.")
+  }
+
   # Extract PCs
   pcs <- reducedDim(sce, pca_name)
   clustering_results <- bluster::clusterRows(pcs, BLUSPARAM, ...)
-  sce[[cluster_name]] <- clustering_results
+  sce[[cluster_column_name]] <- clustering_results
 
   return(sce)
 }
