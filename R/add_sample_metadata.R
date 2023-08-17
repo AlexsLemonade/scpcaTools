44fe42b6c36df2d20cf7e56cb09c8e83e921b802
Ally Hawkins <ally.hawkins@ccdatalab.org>
2023-08-17 15:13:01 -0500

    filter sample metadata within function

--- R/add_sample_metadata.R
+++ R/add_sample_metadata.R
@@ -1,17 +1,15 @@
 #' Add data frame with sample metadata to an SCE object
 #'
 #' @param sce SingleCellExperiment object to add metadata too
-#' @param metadata_df A dataframe with metadata to add to the sce.
-#' @param sample_id_column Name of column in `metadata_df` that contains the sample ids.
-#'   The contents of this column should be equivalent to the `sample_id` in `metadata(sce)`
+#' @param metadata_df A dataframe with metadata to add to the sce. Must contain a
+#'   column named `sample_id`.
 #'
 #' @return SingleCellExperiment object with a `metadata_df` added to the `sample_metadata`
 #'   slot of the metadata list
 #' @export
 #'
 add_sample_metadata <- function(sce,
-                                metadata_df,
-                                sample_id_column){
+                                metadata_df){
 
   # make sure input is sce
   if (!is(sce, "SingleCellExperiment")) {
@@ -24,16 +22,16 @@ add_sample_metadata <- function(sce,
   }
 
   # sample id column should be present in metadata
-  if(!sample_id_column %in% colnames(metadata_df)){
+  if(!"sample_id" %in% colnames(metadata_df)){
     stop("`sample_id_column` not found in `metadata_df`")
   }
 
-  # check that sample id in object corresponds to the sample id column in metadata being added
-  sce_sample_ids <- metadata(sce)$sample_id |>
-    stringr::str_split_1(pattern = ",")
-  metadata_sample_ids <- metadata_df[[sample_id_column]]
+  # filter to relevant sample ids
+  metadata_df <- metadata_df |>
+    dplyr::filter(.data$sample_id %in% metadata(sce)$sample_id)
 
-  if(!all.equal(sort(sce_sample_ids), sort(metadata_sample_ids))){
+  # check that sample id in object corresponds to the sample id column in metadata being added
+  if(!all.equal(sort(metadata_df$sample_id), sort(metadata(sce)$sample_id))){
     stop("Sample ids in SCE object do not match the contents of `sample_id_column` in `metadata_df`")
   }
 
