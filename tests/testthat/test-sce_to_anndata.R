53a7bd1718a06533add8686c21c81327d39a6d77
Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>
2023-09-05 17:05:50 -0500

    update tests

--- tests/testthat/test-sce_to_anndata.R
+++ tests/testthat/test-sce_to_anndata.R
@@ -3,10 +3,13 @@ sce <- sim_sce(n_cells = 100, n_genes = 200, n_empty = 0)
 colData(sce) <- DataFrame("test_column" = sample(0:10, 100, rep = TRUE))
 rowData(sce) <- DataFrame("test_row" = sample(0:10, 200, rep = TRUE))
 
+# define anndata output
+anndata_file <- "test_anndata.h5"
+# quiet messages that will come with zellkonverter
+suppressPackageStartupMessages(library(zellkonverter))
+
+
 test_that("Conversion of SCE to AnnData works as expected", {
-  anndata_file <- "test_anndata.h5"
-  # quiet messages that will come with zellkonverter
-  suppressPackageStartupMessages(library(zellkonverter))
 
   # test that the H5 file is created
   expect_snapshot_file({
@@ -20,7 +23,23 @@ test_that("Conversion of SCE to AnnData works as expected", {
   expect_equal(colnames(colData(sce)), colnames(colData(converted_sce)))
   expect_equal(colnames(rowData(sce)), colnames(rowData(converted_sce)))
 
+  # test that H5 file is created with new assay name
+  # add logcounts
+  logcounts(sce) <- counts(sce)
+  new_anndata_file <- "test_anndata_logcounts.h5"
+  expect_snapshot_file({
+    sce_to_anndata(sce, new_anndata_file, x_assay_name = "logcounts")
+    new_anndata_file
+  })
+
+})
+
+test_that("Conversion of SCE to AnnData fails as expected", {
+
   # check that inputting an improper file name causes a failure
   anndata_bad_file <- "test_anndata"
   expect_error(sce_to_anndata(sce, anndata_bad_file))
+
+  # improper assay name causes a failure
+  expect_error(sce_to_anndata(sce, anndata_file, x_assay_name = "not an assay"))
 })
