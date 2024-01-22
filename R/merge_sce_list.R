7040669fcce2db7dd62e96aae585669201e7d6c9
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2024-01-22 13:44:19 -0500

    probably show two altexp names in the example

--- R/merge_sce_list.R
+++ R/merge_sce_list.R
@@ -50,7 +50,10 @@
 #' \dontrun{
 #' merge_sce_list(
 #'   sce_list = list("sce1" = sce1, "sce2" = sce2),
-#'   retain_altexp_coldata_cols = list("adt" = c("discard", "high.controls"))
+#'   retain_altexp_coldata_cols = list(
+#'     "adt" = c("discard", "high.controls"),
+#'     "other_altexp" = c("first_column", "second_column")
+#'   )
 #' )
 #' }
 #'
