a0a8d5a67cc36aa85344e5a4a85c76f6a5828dd7
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-09-04 17:22:34 -0400

    set seed conditionally in cluster_sce

--- R/cluster_sce.R
+++ R/cluster_sce.R
@@ -7,37 +7,40 @@
 #'   use and any additional clustering options
 #' @param cluster_column_name The name of the column to store the clustering
 #'   results in the SCE; for naming you may want to include the type of clustering
-#'   and k value of centers, e.g. `kmeans_10`
+#'   and k value of centers, e.g., "kmeans_10"
 #' @param seed Seed for reproducibility of clustering results
-#' @param ... Additional arguments to provide to `bluster::clusterRows()`
+#' @param ... Additional arguments to provide to bluster::clusterRows()
 #'
 #' @import SingleCellExperiment
 #'
 #' @return SingleCellExperiment object containing clustering results
 #'
 #' @examples
-#'  \dontrun{
-#'   # Perform K-means clustering with 10 centers
-#'   cluster_sce(sce, "PCA", bluster::KmeansParam(centers = 10), "kmeans_10")
-#'  }
-cluster_sce <- function(sce,
-                        pc_name = "PCA",
-                        BLUSPARAM,
-                        cluster_column_name,
-                        seed = NULL,
-                        ...) {
+#' \dontrun{
+#' # Perform K-means clustering with 10 centers
+#' cluster_sce(sce, "PCA", bluster::KmeansParam(centers = 10), "kmeans_10")
+#' }
+#'
+cluster_sce <- function(
+    sce,
+    pc_name = "PCA",
+    BLUSPARAM,
+    cluster_column_name,
+    seed = NULL,
+    ...) {
   # Set the seed
-  set.seed(seed)
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
 
   # Check that provided sce is indeed an SCE
   if (!is(sce, "SingleCellExperiment")) {
-    stop("Expected a `SingleCellExperiment` object for `sce` argument.")
+    stop("`sce` must be a `SingleCellExperiment` object")
   }
 
   # Check that the PCs are present in the SingleCellExperiment object
   if (!pc_name %in% reducedDimNames(sce)) {
-    stop("The provided `pc_name` cannot be found in the reduced dimensions of
-         the SingleCellExperiment object.")
+    stop("The provided `pc_name` cannot be found in the reduced dimensions of the SingleCellExperiment object.")
   }
 
   # Extract PCs
