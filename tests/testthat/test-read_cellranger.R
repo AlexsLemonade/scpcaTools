8641cd57d73801a111bd2441d5759d11e0d58630
Ally Hawkins <ally.hawkins@ccdatalab.org>
2025-06-11 14:34:02 -0500

    update tests

--- tests/testthat/test-read_cellranger.R
+++ tests/testthat/test-read_cellranger.R
@@ -1,11 +1,12 @@
 dir <- system.file("extdata", package = "scpcaData")
-cellranger_dir <- file.path(dir, "Breast_Cancer_3p_LT/cellranger_cdna")
+cellranger_dir <- file.path(dir, "Breast_Cancer_3p_LT", "cellranger_cdna", "outs")
+h5_file <- file.path(cellranger_dir, "filtered_feature_bc_matrix.h5")
 
 # expected matrix size
 sce_size <- c(19970, 687)
 
 test_that("Check that cellranger import works", {
-  sce <- read_cellranger(cellranger_dir)
+  sce <- read_cellranger(h5_file)
   expect_s4_class(sce, "SingleCellExperiment")
   expect_equal(dim(sce), sce_size)
   # check that column names are barcodes
